60cd4ade9a8bc2fddd36c50b15933cc417bd6351
chandureddys <csreddy.512@gmail.com>
2017-07-08 21:58:35 -0400

    added employee controller

---
+++ src/main/java/com/spring/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.dto.Employee;
+
+@Controller
+public class EmployeeController {
+	
+	@RequestMapping("/employee")
+	public ModelAndView employee(){
+		
+		ModelAndView modelandView = new ModelAndView();
+		
+		Employee employee = new Employee();
+		
+		employee.setId(12);
+		employee.setName("Chandrasekhar");
+		employee.setPosition("Software Engineer");
+		
+		modelandView.addObject("employee", employee);
+		
+		
+		
+		return modelandView;
+		
+		
+	}
+
+}

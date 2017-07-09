a4ace55390cb5a209c18b96434201000eaa851e4
chandureddys <csreddy.512@gmail.com>
2017-07-08 21:08:24 -0400

    modified controller class

--- src/main/java/com/spring/controller/HelloController.java
+++ src/main/java/com/spring/controller/HelloController.java
@@ -1,23 +1,17 @@
 package com.spring.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController {
     
-	
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
-		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "hello world");
-
-		return model;
+	@RequestMapping("/hello")
+	protected ModelAndView hello(){
+		ModelAndView modelandView = new ModelAndView();
+		modelandView.setViewName("hello");
+		return modelandView;
 	}
 
 	
